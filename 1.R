5a210e865f7352b5b3082d327a91845e0f07d5de
PhoebeLiuyf <liuyufeilucky24@163.com>
2024-10-28 00:23:26 +0000

    如果push按钮不能用

--- 1.R
+++ 1.R
@@ -3,4 +3,24 @@ library(janitor)
 
 2+2
 
-##
+'''如果push按钮不能用了
+
+查看是否远程连接
+git remote -v
+
+在terminal中运用后显示
+origin  https://github.com/andrewmaclachlan/example.git (fetch)
+origin  https://github.com/andrewmaclachlan/example.git (push)
+
+设置Github的repo
+git remote add origin https://github.com/andrewmaclachlanc/myrepo.git
+
+#检查当前分支
+git branch -vv
+
+#推送到远程仓库
+git push -u origin main
+
+#更改URL
+git remote set-url origin [enter your cloned URL from GitHub here]
+'''
